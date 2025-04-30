1574e8c10645c39c5f8d6c6f70382709968fe149
EugeneWinter <corvoattano200529@gmail.com>
2025-04-30 23:19:02 +0300

    Четвертый этап выполнен

--- src/Cli.php
+++ src/Cli.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function welcomeUser()
+function welcomeUser(): void
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
