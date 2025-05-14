479ec0af745db58c39e1e91ad4a93191c61c8b9e
EugeneWinter <corvoattano200529@gmail.com>
2025-05-14 03:34:46 +0300

    Попытка 24.4

---
+++ src/Engine.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EugeneWinter\BrainGames\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+const GAME_MESSAGES = [
+    'correct' => 'Correct!',
+    'wrong' => "'%s' is wrong answer ;(. Correct answer was '%s'.",
+    'try_again' => "Let's try again, %s!"
+];
+
+function runGame(callable $getGameData, string $description, string $name): void
+{
+    line($description);
+
+    $roundsCount = 3;
+
+    for ($i = 0; $i < $roundsCount; $i++) {
+        ['question' => $question, 'correctAnswer' => $correctAnswer] = $getGameData();
+        line("Question: %s", $question);
+        $answer = prompt('Your answer');
+
+        if ($answer !== $correctAnswer) {
+            line(GAME_MESSAGES['wrong'], $answer, $correctAnswer);
+            line(GAME_MESSAGES['try_again'], $name);
+            return;
+        }
+        line(GAME_MESSAGES['correct']);
+    }
+
+    line("Congratulations, %s!", $name);
+}
